d6f24179d6c65c7003238be77b3d8843ec4fdfa6
LipikaAggarwal <86905882+LipikaAggarwal@users.noreply.github.com>
2024-08-04 18:36:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-if (window.location.href==="https://lipikaaggarwal.github.io/resume") {
+if (window.location.href == "https://lipikaaggarwal.github.io/resume") {
     window.location.href = "https://example.com"; 
 }
